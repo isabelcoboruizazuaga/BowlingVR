82ba3f069aae0165db5c82f754f6567614d4cbf9
Isa <isabelcoboruizazuaga@gmail.com>
2024-02-15 10:21:22 +0100

    started pin logic

--- Assets/Script/ColisionBolos.cs
+++ Assets/Script/ColisionBolos.cs
@@ -1,9 +1,15 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
 public class ColisionBolos : MonoBehaviour
 {
+    private GameObject[] bolos;
+    private int caidos;
+    private float treshold=90f;
+
+
     // Start is called before the first frame update
     void Start()
     {
@@ -14,5 +20,29 @@ public class ColisionBolos : MonoBehaviour
     void Update()
     {
         
+    }
+
+    private IEnumerator OnTriggerEnter(Collider other)
+    {
+        yield return new WaitForSeconds(3);
+        if (other.tag == "Ball")
+        {
+            Debug.Log("plano de bolos");
+            cuentaCaidos();
+        }
+    }
+
+    private void cuentaCaidos()
+    {
+        bolos = GameObject.FindGameObjectsWithTag("Bolo");
+
+        foreach (GameObject bolo in bolos) {
+            if (bolo.transform.rotation.x > -91f  || bolo.transform.rotation.x < -89f )
+            {
+                caidos++;
+                Destroy(bolo);
+            }
+        }
+        Debug.Log(caidos);
     }
 }
